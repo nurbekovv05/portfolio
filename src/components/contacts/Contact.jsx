106ca1c280f196f3053dadbe0bc6bf906aedf3a2
nurbekovv05 <baevnurbol@gmail.com>
2023-06-22 15:31:15 +0600

    da

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -17,93 +17,94 @@ const Contact = () => {
     };
     return (
         <>
-            <section className="contact section" id="section">
-                <h2 className="section__title">
-                    Get in touch
-                </h2>
-                <span className="section__subtitle">
+            <div id="contact">
+                <section className="contact section" id="section">
+                    <h2 className="section__title">
+                        Get in touch
+                    </h2>
+                    <span className="section__subtitle">
                     Contact Me
                 </span>
 
-                <div className="contact__container container grid">
-                    <div className="contact__content">
-                        <h3 className="contact__title">
-                            Talk to me
-                        </h3>
+                    <div className="contact__container container grid">
+                        <div className="contact__content">
+                            <h3 className="contact__title">
+                                Talk to me
+                            </h3>
 
-                        <div className="contact__info">
-                            <div className="contact__card">
-                                <i className="bx bx-mail-send contact__card-icon"></i>
+                            <div className="contact__info">
+                                <div className="contact__card">
+                                    <i className="bx bx-mail-send contact__card-icon"></i>
 
-                                <h3 className="contact__card-title">
-                                    Email
-                                </h3>
-                                <span className="contact__card-data">baevnurbol@gmail.com</span>
+                                    <h3 className="contact__card-title">
+                                        Email
+                                    </h3>
+                                    <span className="contact__card-data">baevnurbol@gmail.com</span>
 
-                                <a href="malito:baevnurbol@gmail.com" className="contact__button"> Write me{' '}
-                                <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
+                                    <a href="malito:baevnurbol@gmail.com" className="contact__button"> Write me{' '}
+                                        <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
+                                </div>
+
+                                <div className="contact__card">
+                                    <i className="bx bxl-whatsapp contact__card-icon"></i>
+
+                                    <h3 className="contact__card-title">
+                                        Whatsapp
+                                    </h3>
+                                    <span className="contact__card-data">+996 (502) 44-80-05</span>
+
+                                    <a href="https://web.whatsapp.com/" className="contact__button"> Write me{' '}
+                                        <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
+                                </div>
+
+                                <div className="contact__card">
+                                    <i className="bx bxl-messenger contact__card-icon"></i>
+
+                                    <h3 className="contact__card-title">
+                                        Messenger
+                                    </h3>
+                                    <span className="contact__card-data"></span>
+
+                                    <a href="" className="contact__button"> Write me{' '}
+                                        <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
+                                </div>
+
                             </div>
-
-                            <div className="contact__card">
-                                <i className="bx bxl-whatsapp contact__card-icon"></i>
-
-                                <h3 className="contact__card-title">
-                                    Whatsapp
-                                </h3>
-                                <span className="contact__card-data">+996 (502) 44-80-05</span>
-
-                                <a href="https://web.whatsapp.com/" className="contact__button"> Write me{' '}
-                                    <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
-                            </div>
-
-                            <div className="contact__card">
-                                <i className="bx bxl-messenger contact__card-icon"></i>
-
-                                <h3 className="contact__card-title">
-                                    Messenger
-                                </h3>
-                                <span className="contact__card-data"></span>
-
-                                <a href="" className="contact__button"> Write me{' '}
-                                    <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
-                            </div>
-
                         </div>
-                    </div>
-                    <div className="contact__content">
-                        <h3 className="contact__title"> Write me your project
-                        </h3>
-                        <form ref={form} onSubmit={sendEmail} className="contact__form">
-                            <div className="contact__form-div">
-                                <label  className="contact__form-tag">Name</label>
-                                <input type="text" name='name' className='contact__form-input' placeholder='Insert your name'
-                                />
-                            </div>
+                        <div className="contact__content">
+                            <h3 className="contact__title"> Write me your project
+                            </h3>
+                            <form ref={form} onSubmit={sendEmail} className="contact__form">
+                                <div className="contact__form-div">
+                                    <label  className="contact__form-tag">Name</label>
+                                    <input type="text" name='name' className='contact__form-input' placeholder='Insert your name'
+                                    />
+                                </div>
 
-                            <div className="contact__form-div">
-                                <label  className="contact__form-tag">Mail</label>
-                                <input type="email" name='email' className='contact__form-input' placeholder='Insert your email'
-                                />
-                            </div>
+                                <div className="contact__form-div">
+                                    <label  className="contact__form-tag">Mail</label>
+                                    <input type="email" name='email' className='contact__form-input' placeholder='Insert your email'
+                                    />
+                                </div>
 
-                            <div className="contact__form-div contact__form-area">
-                                <label  className="contact__form-tag">Project</label>
-                                <textarea name="project"  cols="30" rows="10"
-                                          className='contact__form-input' placeholder='Write your project'></textarea>
-                            </div>
+                                <div className="contact__form-div contact__form-area">
+                                    <label  className="contact__form-tag">Project</label>
+                                    <textarea name="project"  cols="30" rows="10"
+                                              className='contact__form-input' placeholder='Write your project'></textarea>
+                                </div>
 
-                            <button className="button button__flex">
-                                Send Message
-                                <svg
-                                    className="button__icon"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                >
-                                    <path
-                                        d="M14.2199 21.9352C13.0399
+                                <button className="button button__flex">
+                                    Send Message
+                                    <svg
+                                        className="button__icon"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                    >
+                                        <path
+                                            d="M14.2199 21.9352C13.0399
                             21.9352 11.3699 21.1052 10.0499
                             17.1352L9.32988 14.9752L7.16988
                             14.2552C3.20988 12.9352 2.37988
@@ -127,21 +128,22 @@ const Contact = () => {
                                8.16525C20.3099 6.62525 20.2199
                                5.36525 19.5699 4.71525C18.9199
                                 4.06525 17.6599 3.98525 16.1299 4.49525L7.63988 7.33525Z"
-                                        fill="var(--container-color)"
-                                    ></path>
-                                    <path
-                                        d="M10.11 14.7052C9.92005 14.7052 9.73005 14.6352 9.58005 14.4852C9.29005 14.1952 9.29005 13.7152 9.58005 13.4252L13.16 9.83518C13.45 9.54518 13.93 9.54518 14.22 9.83518C14.51 10.1252 14.51 10.6052 14.22 10.8952L10.64 14.4852C10.5 14.6352 10.3 14.7052 10.11 14.7052Z"
-                                        fill="var(--container-color)"
-                                    ></path>
-                                </svg>
-                            </button>
+                                            fill="var(--container-color)"
+                                        ></path>
+                                        <path
+                                            d="M10.11 14.7052C9.92005 14.7052 9.73005 14.6352 9.58005 14.4852C9.29005 14.1952 9.29005 13.7152 9.58005 13.4252L13.16 9.83518C13.45 9.54518 13.93 9.54518 14.22 9.83518C14.51 10.1252 14.51 10.6052 14.22 10.8952L10.64 14.4852C10.5 14.6352 10.3 14.7052 10.11 14.7052Z"
+                                            fill="var(--container-color)"
+                                        ></path>
+                                    </svg>
+                                </button>
 
 
 
-                        </form>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
         </>
     );
 };
